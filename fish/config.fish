2de5e8e43ff359a3f3f8cc498c4ff0f256dd9963
Sarah Traut <sarah.traut@pm.me>
2025-12-18 14:26:42 +0100

    initial config

---
+++ fish/config.fish
@@ -0,0 +1,13 @@
+set -gx EDITOR nvim
+function y
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file="$tmp"
+    if read -z cwd <"$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        builtin cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
+end
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+oh-my-posh init fish --config ~/.config/OhMyPosh/leaf.omp.json | source
